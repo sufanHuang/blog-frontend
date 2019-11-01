800f859c630b26b86eca85a456bf1b833558e590
sufanHuang <sufanhk@gmail.com>
2019-11-01 14:40:18 -0500

    Adding css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+//import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
